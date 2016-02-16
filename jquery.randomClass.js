01f9d8706edd66558cba82546ea70097126aa3ab
Chris Johnson <chris@redscriptdesign.com>
2016-02-15 22:14:56 -0800

    Clean up code and fix customArray issues

--- jquery.randomClass.js
+++ jquery.randomClass.js
@@ -1,76 +1,73 @@
-(function( $ ) {
+(function($) {
 
-$.fn.randomClass = function(options){
-	var defaults = {
-		count: 0,
-		repeat: false,
-		customClass: "randomClass",
-		customArray: [],
-		after: function(){}
-	}
+    $.fn.randomClass = function(options) {
+        var defaults = {
+                count: 0,
+                repeat: false,
+                customClass: "randomClass",
+                customArray: [],
+                after: function() {}
+            },
+            settings = $.extend({}, defaults, options),
+            elsLength = this.length,
+            classNums = [],
+            count = settings.count,
+            customArrayLen = settings.customArray.length;
 
-	var settings = $.extend({}, defaults, options );
 
-	var	elsLength = this.length;
-		classNums = [],
-		count = settings.count,
-		customArrayLen = settings.customArray.length;
+        if (customArrayLen > 0) {
+            count = customArrayLen
 
-		if (customArrayLen > 0){
-			if (customArrayLen < elsLength ){
-				return this.each(function(){
-				count = customArrayLen;
-				var randomNum = Math.floor(Math.random() * count);
-				var currentItem = $(this);
-				currentItem.addClass(settings.customArray[randomNum]);
-				count--;
-			}, settings.after());
+            if (count < elsLength) {
+                return this.each(function() {
+                    var randomNum = Math.floor(Math.random() * count),
+                        currentItem = $(this);
 
-		} else if (customArrayLen == elsLength){
-			count = elsLength;
-			return this.each(function(){
-			var randomNum = Math.floor(Math.random() * count),
-			selectedClass = String(settings.customArray.splice(randomNum, 1));
-			var currentItem = $(this);
-			currentItem.addClass(selectedClass);
-			count--;
-		}, settings.after());
-		}
+                    currentItem.addClass(settings.customArray[randomNum]);
+                }, settings.after());
 
-		}
+            } else {
+                return this.each(function() {
+                    var randomNum = Math.floor(Math.random() * count),
+                        selectedClass = String(settings.customArray.splice(randomNum, 1)),
+                        currentItem = $(this);
 
-	if (count <= 0 || isNaN(count) || count < elsLength){
-		count = elsLength;
-		for(i = 0; i< count; i++){
-				classNums.push(i);
-		}
-	}
+                    currentItem.addClass(selectedClass);
+                    count--;
+                }, settings.after());
+            }
 
-	else{
-		for(i = 0; i< count; i++){
-			classNums.push(i);
-		}
-	}
+        }
 
-	if (settings.repeat === false){
-		return this.each(function(){
-			var randomNum = Math.floor(Math.random() * count),
-			selectedNum = classNums.splice(randomNum, 1);
-			var currentItem = $(this);
-			currentItem.addClass(settings.customClass + selectedNum);
-			count--;
-		}, settings.after());
+        if (count <= 0 || isNaN(count) || count < elsLength) {
+            count = elsLength;
+        }
 
-	} else {
-		return this.each(function(){
-			var randomNum = Math.floor(Math.random() * count);
-			var currentItem = $(this);
-			currentItem.addClass(settings.customClass + randomNum);
-			count--;
-		},settings.after());
-	}
-	
+        for (i = 0; i < count; i++) {
+            classNums.push(i);
+        }
 
-};
 
-}( jQuery ));
+        if (settings.repeat === false) {
+            return this.each(function() {
+                var randomNum = Math.floor(Math.random() * count),
+                    selectedNum = classNums.splice(randomNum, 1),
+                    currentItem = $(this);
+
+                currentItem.addClass(settings.customClass + selectedNum);
+                count--;
+            }, settings.after());
+
+        } else {
+            return this.each(function() {
+                var randomNum = Math.floor(Math.random() * count),
+                    currentItem = $(this);
+
+                currentItem.addClass(settings.customClass + randomNum);
+            }, settings.after());
+        }
+
+
+    };
+
+}(jQuery));
